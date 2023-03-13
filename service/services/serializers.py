bdd0a6e14241e1cc6d3c6fb278f019d407d28dc4
Victor <rojcovictor1@gmail.com>
2023-03-13 21:56:14 +0200

    Added logging and prefetch to the subscriptions view

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from services.models import Subscription
+from services.models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = '__all__'
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
 
@@ -14,5 +21,6 @@ class SubscriptionSerializer(serializers.ModelSerializer):
             'plan_id',
             'client_name',
             'email',
+            'plan',
         )
 
